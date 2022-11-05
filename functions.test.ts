11ba3ab65c1d8f8e501b3f3d63e6180c4ce82600
Nate Ensign <dixienate@gmail.com>
2022-11-05 15:15:04 -0600

    part 3 complete

--- functions.test.ts
+++ functions.test.ts
@@ -1,5 +1,15 @@
 const {shuffleArray} = require('./utils')
 
+const testArr1 = [1,2,3,4]
+
 describe('shuffleArray should', () => {
-    // CODE HERE
+   
+    test('return an array', () => {
+        expect(Array.isArray(shuffleArray(testArr1))).toBe(true)
+    }),
+
+    test('return an array of the same length as the input', () => {
+        expect(shuffleArray(testArr1)).toHaveLength(testArr1.length)
+    })
+    
 })
